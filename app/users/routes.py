3f28d520a94cbfdd2953c28858b4295f657253cb
rodres101 <rodrigosergio93@gmail.com>
2024-03-30 08:44:34 -0400

    added tailwind table

--- app/users/routes.py
+++ app/users/routes.py
@@ -3,6 +3,23 @@ from app.users import bp
 
 @bp.route('/')
 def index():
-  return render_template('users/index.html')
+  data = [
+    {
+      "name" : "Sergio Rodrigo Cardenas Rivera",
+      "user_name": "sergio.cardenas",
+      "last_connection": "2023-03-29",
+    },
+    {
+      "name" : "Miriam Lucana",
+      "user_name": "miriam.lucana",
+      "last_connection": "2023-03-29",
+    },
+    {
+      "name" : "Pedro Peredo",
+      "user_name": "pedro.peredo",
+      "last_connection": "2023-03-29",
+    },
+  ]
+  return render_template('users/index.html', data=data)
   # else:
   #   return redirect(url_for('main.show_the_login_form'))
